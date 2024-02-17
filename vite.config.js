d4972fc853a44c274b71c547045b9b5e382d148c
XuBaoPing <xubaoping@dbg.com.cn>
2024-02-17 15:13:58 +0800

    构建补丁

--- vite.config.js
+++ vite.config.js
@@ -21,5 +21,14 @@ export default defineConfig(async () => ({
     watch: {
       ignored: ["**/Rust/**"],
     },
-  }
+  },
+  envPrefix: ['VITE_', 'TAURI_PLATFORM', 'TAURI_ARCH', 'TAURI_FAMILY', 'TAURI_PLATFORM_VERSION', 'TAURI_PLATFORM_TYPE', 'TAURI_DEBUG'],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // 为调试构建生成源代码映射 (sourcemap)
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
 }));
